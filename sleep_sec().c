f6ce4285e4fcaf00112d5205c4b25f2e43a8e9b8
mitsosf <dimitris.frangiadakis@hotmail.com>
2015-11-29 18:40:11 +0200

    Sleep_sec() function

--- sleep_sec().c
+++ sleep_sec().c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <time.h>  // Used in lines 10 and 13
 
 
 
 void sleep_sec(int seconds){
 	int x,y;
 	x=0;
-	y=time(NULL);
+	y=time(NULL);  //Using time(NULL) function to count in seconds and make it system-independent
 	
-	while((x-y)<seconds)
+	while((x-y)<seconds) //Counting
 		x=time(NULL);
 		
 }
 
-int main(int argc, char *argv[]) {
-	printf("Geia");
+int main(int argc, char *argv[]) {  //test
+	printf("Hello,");
 	sleep_sec(5);
-	printf(" sou\n");
+	printf(" world!\n");
 	system("pause");
 	return 0;
 }
